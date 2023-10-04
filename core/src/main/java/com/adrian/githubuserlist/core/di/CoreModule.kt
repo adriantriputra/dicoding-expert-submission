4e15f89cada04bb0d341604cc112fee594e056c1
adriantriputra <adriantriputra.97@gmail.com>
2023-10-04 16:55:00 +0700

    added certificate pinner

--- core/src/main/java/com/adrian/githubuserlist/core/di/CoreModule.kt
+++ core/src/main/java/com/adrian/githubuserlist/core/di/CoreModule.kt
@@ -8,6 +8,7 @@ import com.adrian.githubuserlist.core.data.source.remote.RemoteDataSource
 import com.adrian.githubuserlist.core.data.source.remote.network.ApiService
 import com.adrian.githubuserlist.core.domain.repository.IUserRepository
 import com.adrian.githubuserlist.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
@@ -18,6 +19,12 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .add(hostname, "sha256/e0IRz5Tio3GA1Xs4fUVWmH1xHDiH2dMbVtCBSkOIdqM=")
+            .add(hostname, "sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=")
+            .build()
         val authInterceptor = Interceptor { chain ->
             val request = chain.request()
             val requestHeaders = request.newBuilder()
@@ -29,6 +36,7 @@ val networkModule = module {
             .addInterceptor(authInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
